c8096c59fabd35dc8058abec9322cd01a02d6ef3
Joao Victor Albuquerque <93137253+jvoaojvictor@users.noreply.github.com>
2024-05-08 11:44:00 -0300

    fix: Constants.java

--- src/main/java/my/restaurant/utils/Constants.java
+++ src/main/java/my/restaurant/utils/Constants.java
@@ -1,6 +1,6 @@
 package my.restaurant.utils;
 
 public class Constants {
-    public static final String PAGE_TITLE = "pageTitle";
-    public static final String LOGIN_PAGE = "loginPage"; 
+    public static final String PageTitle = "pageTitle";
+    public static final String LoginPage = "loginPage"; 
 }
