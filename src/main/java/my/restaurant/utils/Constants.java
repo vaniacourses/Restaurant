17856db5a8e4bbbc6f249b2c3a1ba17bbedc3a33
Victor-CSM <victorcsm@id.uff.br>
2024-05-08 10:57:03 -0300

    Var

--- src/main/java/my/restaurant/utils/Constants.java
+++ src/main/java/my/restaurant/utils/Constants.java
@@ -2,5 +2,5 @@ package my.restaurant.utils;
 
 public class Constants {
     public static final String PAGE_TITLE = "pageTitle";
-    public static final String LOGIN_PAGE = "loginPage";
+    public static final String LOGIN_PAGE = "loginPage"; 
 }
