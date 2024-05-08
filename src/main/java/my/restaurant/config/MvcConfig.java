9cb8a8af4e30fa945938491da30cd53ffd9341d5
Victor-CSM <victorcsm@id.uff.br>
2024-05-08 14:13:26 -0300

    Update MvcConfig.java
    Método não implementado, comentários

--- src/main/java/my/restaurant/config/MvcConfig.java
+++ src/main/java/my/restaurant/config/MvcConfig.java
@@ -7,5 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public MvcConfig() {
+	// Este método está vazio porque não requer configuração adicional no momento.
+    // Se configurações adicionais forem necessárias no futuro, elas podem ser adicionadas aqui.
+    // Por enquanto, não há ação necessária.
     }
+    
 }
